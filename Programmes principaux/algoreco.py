547aed575e20a46bb6030d0fba1fa078941e3000
juancarlos-06 <58878109+juancarlos-06@users.noreply.github.com>
2021-04-08 14:40:18 +0200

    Update algoreco.py

--- Programmes principaux/algoreco.py
+++ Programmes principaux/algoreco.py
@@ -3,28 +3,31 @@ import numpy
 import os
 
 path = '/home/pi/Desktop/base3'
+#chemin de la base de données
+
 orb = cv2.ORB_create(nfeatures=1000)
 
 #### Import Images
 images = []
 classNames = []
 myList = os.listdir(path)
+
 #affiche le nombre d'images dans la base de données
 print('Total Classes Detected', len(myList))
 
 #enleve le .png dans le nom des images
-for cl in myList:
-    imgCur = cv2.imread(f'{path}/{cl}', 0)
+for num in myList:
+    imgCur = cv2.imread(f'{path}/{num}', 0)
     images.append(imgCur)
-    classNames.append(os.path.splitext(cl)[0])
+    classNames.append(os.path.splitext(num)[0])
 print(classNames)
 
 #Trouve les descriptors/keypoints des images et les ajoute a desList
 #un descriptor ou keypoints est un element permettant de caractériser une image
-def findDes(images):
+def findDescriptor(images):
     desList = []
-    for img in images:
-        kp, des = orb.detectAndCompute(img, None)
+    for i in images:
+        kp, des = orb.detectAndCompute(i, None)
         desList.append(des)
     return desList
 
@@ -37,6 +40,7 @@ def findID(img, desList, thres = 3):
     matchList = []
     finalVal = -1
     try:
+        #compare le descriptor d'une image avec la liste de descriptors de la bdd
         for des in desList:
             matches = bf.knnMatch(des, des2, k=2)
             good = []
@@ -49,6 +53,7 @@ def findID(img, desList, thres = 3):
     # print(matchList)
     if len(matchList) != 0:
         if max(matchList) > thres:
+            #retourne l'image avec le plus de similitudes 
             finalVal = matchList.index(max(matchList))
     return finalVal
 
@@ -75,15 +80,17 @@ def reco():
     
     success, img2 = cap.read()
     imgOriginal = img2.copy()
+    #copie l'image de la caméra
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    #la transforme en nuances de gris
     img2 = cv2.GaussianBlur(img2, (7,7), 0)
+    #la floute
 
     id = findID(img2, desList)
     if id != -1:
+        #si la fonction findID retourne un resultat coherent, l'affiche sur le flux camera
         cv2.putText(imgOriginal, classNames[id], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
         sucessimg.append(classNames[id])
-        #print(classNames[id])
-        #print(chiffrefinal(sucessimg))
     
     cv2.imshow('img2', imgOriginal)
     
